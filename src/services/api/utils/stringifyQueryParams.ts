2cc5940cf4dbe3cab952e2cc5684843c6b28178f
Artur Mędrygał <info@heseya.com>
2022-02-25 09:37:06 +0100

    Improved params stringify

--- src/services/api/utils/stringifyQueryParams.ts
+++ src/services/api/utils/stringifyQueryParams.ts
@@ -1,11 +1,20 @@
 import queryString from 'query-string'
 import flatten from 'flat'
+import { isBoolean } from 'lodash'
 
 import { DefaultParams } from '../types/DefaultParams'
 
+const transformBooleanParams = (params: DefaultParams): DefaultParams => {
+  return Object.fromEntries(
+    Object.entries(params).map(([key, value]) => [key, isBoolean(value) ? +value : value]),
+  )
+}
+
 export const stringifyQueryParams = (params: DefaultParams): string => {
-  const flatParams = flatten(params, { safe: true }) as Record<string, any>
-  return queryString.stringify(flatParams, {
+  const flattenedParams = flatten<DefaultParams, DefaultParams>(params, { safe: true })
+  const transformedParams = transformBooleanParams(flattenedParams)
+
+  return queryString.stringify(transformedParams, {
     arrayFormat: 'bracket',
     skipNull: true,
   })
--- src/services/api/utils/tests/stringifyQueryParams.test.ts
+++ src/services/api/utils/tests/stringifyQueryParams.test.ts
@@ -4,9 +4,14 @@ describe('stringifyQueryParams', () => {
   it('should transform simple object to string', () => {
     expect(stringifyQueryParams({ sort: 'asc' })).toBe('sort=asc')
     expect(stringifyQueryParams({ price: 252 })).toBe('price=252')
-    expect(stringifyQueryParams({ sort: 'asc', available: true })).toBe('available=true&sort=asc')
+    expect(stringifyQueryParams({ sort: 'asc', set: 'yes' })).toBe('set=yes&sort=asc')
     expect(stringifyQueryParams({ sort: 'asc', available: undefined })).toBe('sort=asc')
     expect(stringifyQueryParams({ test: null })).toBe('')
+  })
+
+  it('should transform booleans fields into 0/1 values', () => {
+    expect(stringifyQueryParams({ avilable: true })).toBe('avilable=1')
+    expect(stringifyQueryParams({ avilable: false })).toBe('avilable=0')
   })
 
   it('should transform array fields into arrays strings', () => {
@@ -18,7 +23,7 @@ describe('stringifyQueryParams', () => {
     // expect(stringifyQueryParams({ attribute: {} })).toBe('')
     expect(stringifyQueryParams({ attribute: { cpu: 'i5' } })).toBe('attribute.cpu=i5')
     expect(stringifyQueryParams({ attribute: { cpu: 'i5', data: true } })).toBe(
-      'attribute.cpu=i5&attribute.data=true',
+      'attribute.cpu=i5&attribute.data=1',
     )
     expect(stringifyQueryParams({ attribute: { price: { max: 100, min: 2 } } })).toBe(
       'attribute.price.max=100&attribute.price.min=2',
