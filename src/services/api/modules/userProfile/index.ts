55396cadb15ab04114f4f0f03937da8e08bd64f2
Artur Mędrygał <info@heseya.com>
2022-02-25 13:42:09 +0100

    HES-905 added user orders service

--- src/services/api/modules/userProfile/index.ts
+++ src/services/api/modules/userProfile/index.ts
@@ -2,7 +2,12 @@ import { HeseyaResponse } from '../../../../interfaces/Response'
 import { User } from '../../../../interfaces/User'
 import { App } from '../../../../interfaces/App'
 import { ServiceFactory } from '../../types/Service'
+import { Order, OrderList } from '../../../../interfaces/Order'
+
 import { createTwoFactorAuthService, TwoFactorAuthService } from './twoFactorAuth'
+import { createGetListRequest, createGetOneRequest } from '../../utils/requests'
+import { GetEntityRequest, getOneEntityRequest } from '../../types/Requests'
+import { OrdersListParams } from '../orders'
 
 export interface UserProfileService {
   /**
@@ -15,6 +20,18 @@ export interface UserProfileService {
    * Change logged user password.
    */
   changePassword(payload: { currentPassword: string; newPassword: string }): Promise<true>
+
+  Orders: {
+    /**
+     * Get list of user owned orders.
+     */
+    get: GetEntityRequest<OrderList, OrdersListParams>
+
+    /**
+     * Get user own order by its ID.
+     */
+    getById: getOneEntityRequest<Order>
+  }
 
   TwoFactorAuthentication: TwoFactorAuthService
 }
@@ -35,4 +52,9 @@ export const createUserProfileService: ServiceFactory<UserProfileService> = (axi
   },
 
   TwoFactorAuthentication: createTwoFactorAuthService(axios),
+
+  Orders: {
+    get: createGetListRequest<OrderList>(axios, 'orders/my'),
+    getById: createGetOneRequest<Order>(axios, 'orders/my', { byId: true }),
+  },
 })
