3a0267b625d5418b98a456b795b0a032c8b136d5
Artur Mędrygał <artur@heseya.com>
2022-04-28 10:25:34 +0200

    HES-1209 changed banners interfaces

--- src/interfaces/Banner.ts
+++ src/interfaces/Banner.ts
@@ -4,22 +4,32 @@ import { UUID } from './UUID'
 export interface Banner {
   id: UUID
   slug: string
-  url: string
   name: string
   active: boolean
-  responsive_media: ResponsiveMedia[]
+  banner_media: BannerMedia[]
 }
 
-export type ResponsiveMedia = { min_screen_width: number; media: CdnMedia }[]
+export interface BannerMedia {
+  id: UUID
+  order: number
+  url: string | null
+  title: string | null
+  subtitle: string | null
+  media: { min_screen_width: number; media: CdnMedia }[]
+}
 
 export interface BannerCreateDto {
   slug: string
-  url: string
   name: string
   active: boolean
-  responsive_media: ResponsiveMediaCreateDto[]
+  banner_media: BannerMediaCreateDto[]
 }
 export type BannerUpdateDto = BannerCreateDto
 
-export type ResponsiveMediaCreateDto = { min_screen_width: number; media: UUID }[]
-export type ResponsiveMediaUpdateDto = ResponsiveMediaCreateDto
+export interface BannerMediaCreateDto {
+  url: string | null
+  title: string | null
+  subtitle: string | null
+  media: { min_screen_width: number; media: UUID }[]
+}
+export type BannerMediaUpdateDto = Partial<BannerMediaCreateDto>
