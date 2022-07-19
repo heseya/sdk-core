dc544b6435b214eeb72e89188d0528a86d444f17
Artur Mędrygał <artur@heseya.com>
2022-07-19 10:52:50 +0200

    Revert "extended heseya service factory"
    This reverts commit 0c248b95be23daf29c7081c2899454d32b37c0d3.

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios'
+import { AxiosInstance } from 'axios'
 
 import { createAuthService } from './modules/auth'
 import { createUserProfileService } from './modules/userProfile'
@@ -27,49 +27,8 @@ import { createSchemasService } from './modules/schema'
 import { createAnalyticsService } from './modules/analytics'
 import { createConsentsService } from './modules/consents'
 
-const createHeseyaApiServiceFromAxios = (axiosInstance: AxiosInstance) => ({
-  Analytics: createAnalyticsService(axiosInstance),
-  Apps: createAppsService(axiosInstance),
-  Auth: createAuthService(axiosInstance),
-  Banners: createBannersService(axiosInstance),
-  UserProfile: createUserProfileService(axiosInstance),
-  Roles: createRolesService(axiosInstance),
-  Users: createUsersService(axiosInstance),
-  Products: createProductsService(axiosInstance),
-  Schemas: createSchemasService(axiosInstance),
-  Tags: createTagsService(axiosInstance),
-  ProductSets: createProductSetsService(axiosInstance),
-  Warehouse: createWarehouseService(axiosInstance),
-  Attributes: createAttributesService(axiosInstance),
-  Pages: createPagesService(axiosInstance),
-  Sales: createSalesService(axiosInstance),
-  Coupons: createCouponsService(axiosInstance),
-  Orders: createOrdersService(axiosInstance),
-  OrderStatuses: createOrderStatusesService(axiosInstance),
-  ShippingMethods: createShippingMethodsService(axiosInstance),
-  PackagesTemplates: createPackagesTemplatesService(axiosInstance),
-  GlobalSeo: createGlobalSeoService(axiosInstance),
-  PaymentMethods: createPaymentMethodsService(axiosInstance),
-  Settings: createSettingsService(axiosInstance),
-  Media: createMediaService(axiosInstance),
-  Webhooks: createWebhooksService(axiosInstance),
-  Consents: createConsentsService(axiosInstance),
-})
-
-const createHeseyaApiServiceFromBaseUrl = (baseURL: string) =>
-  createHeseyaApiServiceFromAxios(axios.create({ baseURL }))
-
-export type HeseyaApiService = ReturnType<typeof createHeseyaApiServiceFromAxios>
-
-type HeseyaApiServiceFactory = {
-  (axios: AxiosInstance): HeseyaApiService
-  (baseURL: string): HeseyaApiService
-}
-
 /**
  * Factory to create whole Heseya e-commerce API service
- *
- * You can only pass the Heseya BaseURL to create the service, but it is recommended to use own Axios instance
  *
  * Why not use the default axios instance?
  * Because, user may want to extend axios instance with some middlewares/interceptors (ex. for user token refreshment)
@@ -81,11 +40,33 @@ type HeseyaApiServiceFactory = {
  * @example
  * heseya.Products.get() // Return all products
  */
-export const createHeseyaApiService: HeseyaApiServiceFactory = (axiosOrBaseUrl) => {
-  if (!axiosOrBaseUrl) throw new Error('Axios instance or base URL is required')
+export const createHeseyaApiService = (axios: AxiosInstance) => ({
+  Analytics: createAnalyticsService(axios),
+  Apps: createAppsService(axios),
+  Auth: createAuthService(axios),
+  Banners: createBannersService(axios),
+  UserProfile: createUserProfileService(axios),
+  Roles: createRolesService(axios),
+  Users: createUsersService(axios),
+  Products: createProductsService(axios),
+  Schemas: createSchemasService(axios),
+  Tags: createTagsService(axios),
+  ProductSets: createProductSetsService(axios),
+  Warehouse: createWarehouseService(axios),
+  Attributes: createAttributesService(axios),
+  Pages: createPagesService(axios),
+  Sales: createSalesService(axios),
+  Coupons: createCouponsService(axios),
+  Orders: createOrdersService(axios),
+  OrderStatuses: createOrderStatusesService(axios),
+  ShippingMethods: createShippingMethodsService(axios),
+  PackagesTemplates: createPackagesTemplatesService(axios),
+  GlobalSeo: createGlobalSeoService(axios),
+  PaymentMethods: createPaymentMethodsService(axios),
+  Settings: createSettingsService(axios),
+  Media: createMediaService(axios),
+  Webhooks: createWebhooksService(axios),
+  Consents: createConsentsService(axios),
+})
 
-  if (typeof axiosOrBaseUrl === 'string') {
-    return createHeseyaApiServiceFromBaseUrl(axiosOrBaseUrl)
-  }
-  return createHeseyaApiServiceFromAxios(axiosOrBaseUrl)
-}
+export type HeseyaApiService = ReturnType<typeof createHeseyaApiService>
