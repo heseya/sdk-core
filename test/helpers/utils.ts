be977235490d9f70c8d5d381819fdbfe553039a1
Artur Mędrygał <artur@heseya.com>
2022-07-25 15:10:23 +0200

    test: Added more tests

--- test/helpers/utils.ts
+++ test/helpers/utils.ts
@@ -1,3 +1,5 @@
+import { HeseyaResponse } from '../../src'
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const resolveInRandomTime = <FuncType extends (...args: any) => any>(
   cb: FuncType,
@@ -10,3 +12,11 @@ export const resolveInRandomTime = <FuncType extends (...args: any) => any>(
       // The greater the spread, the higher chance of token being refreshed before some of the initial requests fails
     }, Math.random() * (maxTime - minTime) + minTime),
   )
+
+export const makeDummyHeseyaResponse = <T>(data: T): HeseyaResponse<T> => ({
+  data,
+  meta: {
+    currency: { name: 'pln', symbol: 'pln', decimals: 2 },
+    language: { symbol: 'pl' },
+  },
+})
