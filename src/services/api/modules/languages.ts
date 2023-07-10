dcda21065baa4cb0d4fc00b69b0571df93797768
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-07-10 16:26:42 +0200

    feat: added languages typings and crud

---
+++ src/services/api/modules/languages.ts
@@ -0,0 +1,36 @@
+import { CrudService, ServiceFactory } from '../types/Service'
+import {
+  createDeleteRequest,
+  createGetListRequest,
+  createPatchRequest,
+  createPostRequest,
+} from '../utils/requests'
+
+import { createEntityMetadataService, EntityMetadataService } from './metadata'
+import { MetadataParams, PaginationParams } from '../types/DefaultParams'
+import { Language, LanguageCreateDto, LanguageUpdateDto } from '../../../interfaces/Language'
+import { createEntityAuditsService, EntityAuditsService } from './audits'
+import { UUID } from '../../../interfaces/UUID'
+
+type LanguagesListParams = MetadataParams & PaginationParams & { ids?: UUID[] }
+
+export interface LanguagesService
+  extends Omit<
+      CrudService<Language, Language, LanguageCreateDto, LanguageUpdateDto, LanguagesListParams>,
+      'getOneBySlug' | 'getOne'
+    >,
+    EntityMetadataService,
+    EntityAuditsService<Language> {}
+
+export const createLanguagesService: ServiceFactory<LanguagesService> = (axios) => {
+  const route = 'statuses'
+  return {
+    get: createGetListRequest(axios, route),
+    create: createPostRequest(axios, route),
+    update: createPatchRequest(axios, route),
+    delete: createDeleteRequest(axios, route),
+
+    ...createEntityMetadataService(axios, route),
+    ...createEntityAuditsService(axios, route),
+  }
+}
