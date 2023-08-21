c46c0c7fb2d732e7fa1af7447e4d9a7106c0b5d7
Artur Mędrygał <artur.medrygal@escolasoft.com>
2023-08-21 12:51:09 +0200

    feat: updated payment interface

--- src/interfaces/Payments.ts
+++ src/interfaces/Payments.ts
@@ -1,12 +1,14 @@
+import { StrNumber } from './Number'
 import { UUID } from './UUID'
 export interface OrderPayment {
   id: UUID
   external_id: UUID
   method: string
-  method_id: null | UUID
+  method_id: UUID | null
   status: PaymentStatus
-  amount: number
-  redirect_url: string
+  amount: StrNumber
+  currency: string
+  redirect_url: string | null
   continue_url: string
   date: string
 }
@@ -22,7 +24,8 @@ export interface Payment {
   external_id: string
   method_id: UUID // PaymentMethod.id
   status: PaymentStatus
-  amount: number
+  amount: StrNumber
+  currency: string
 }
 
 export interface PaymentCreateDto {
@@ -30,7 +33,7 @@ export interface PaymentCreateDto {
   method_id: UUID // PaymentMethod.id
   order_id: UUID // Order.id
   status: PaymentStatus
-  amount: number
+  amount: StrNumber
 }
 
 export type PaymentUpdateDto = Partial<PaymentCreateDto>
