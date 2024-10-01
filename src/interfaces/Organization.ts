bc7ba19dd7e73987ef6d5a0e46286160bd4a4da9
Mateusz Cholcha <mateusz.cholcha@escolasoft.com>
2024-10-01 14:58:39 +0200

    feat: add shipping)address to organization

--- src/interfaces/Organization.ts
+++ src/interfaces/Organization.ts
@@ -1,5 +1,5 @@
 import { UUID } from './UUID'
-import { Address } from './Address'
+import { Address, UserSavedAddress } from './Address'
 import { SalesChannelListed } from './SalesChannel'
 import { OrganizationConsent, OrganizationConsentDto } from './Consent'
 
@@ -61,6 +61,7 @@ export interface OrganizationListed {
 
 export interface Organization extends OrganizationListed {
   consents: OrganizationConsent[]
+  shipping_address: UserSavedAddress[]
 }
 
 /**
